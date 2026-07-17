7bd30042bea1bfc0feb193700d5379b90c705340
Jan Tvrdík <jan.tvrdik@shipmonk.com>
2026-07-17 15:09:57 +0200

    Apply suggestion from @JanTvrdik

--- coverage-guard.php
+++ coverage-guard.php
@@ -7,9 +7,7 @@ use ShipMonk\CoverageGuard\Rule\EnforceCoverageForMethodsRule;
 $config = new Config();
 
 // Every method must be fully covered, except for lines that throw LogicException.
-$config->addExecutableLineExcluder(new IgnoreThrowNewExceptionLineExcluder([
-    LogicException::class,
-]));
+$config->addExecutableLineExcluder(new IgnoreThrowNewExceptionLineExcluder([LogicException::class]));
 $config->addRule(new EnforceCoverageForMethodsRule(requiredCoveragePercentage: 100));
 
 return $config;
